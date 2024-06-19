710f72497ed6a3092d6135daccbcb03bd7c39dee
kixixixixi <kixixixixi@gmail.com>
2024-06-20 01:23:21 +0900

    Add uid

--- app/api/house/route.ts
+++ app/api/house/route.ts
@@ -1,5 +1,7 @@
 import { NextRequest, NextResponse } from "next/server"
 import { prisma } from "lib/db"
+import { Prisma } from "@prisma/client"
+import { fetchAltitude } from "@/lib/request"
 
 export const GET = async (request: NextRequest): Promise<NextResponse> => {
   const houseList = await prisma.house.findMany({
@@ -11,10 +13,20 @@ export const GET = async (request: NextRequest): Promise<NextResponse> => {
 }
 
 export const POST = async (request: NextRequest): Promise<NextResponse> => {
-  const body = await request.json()
+  const body: Prisma.HouseCreateInput = await request.json()
+  const altitude = (await fetchAltitude({ ...body })) ?? 0
   const house = await prisma.house.create({
     data: {
       ...body,
+      uid: `${Math.floor(body.latitude)}${
+        body.latitude.toPrecision(8).split(".")[1]
+      }${Math.floor(body.longitude)}${
+        body.longitude.toPrecision(9).split(".")[1]
+      }${("000" + Math.floor(altitude)).slice(-4)}${(
+        altitude.toPrecision(6) + "00"
+      )
+        .split(".")[1]
+        .slice(0, 2)}`,
     },
   })
   return NextResponse.json({
--- lib/request.ts
+++ lib/request.ts
@@ -49,3 +49,22 @@ export const searchAddress = async ({
     }
   }
 }
+
+export const fetchAltitude = async ({
+  longitude,
+  latitude,
+}: {
+  longitude: number
+  latitude: number
+}): Promise<number | undefined> => {
+  const response = await ky.get(
+    `https://cyberjapandata2.gsi.go.jp/general/dem/scripts/getelevation.php?lon=${longitude}&lat=${latitude}`
+  )
+  if (response.ok) {
+    const { elevation } = await response.json<{
+      elevation: number
+      hsrc: string
+    }>()
+    return elevation
+  }
+}
