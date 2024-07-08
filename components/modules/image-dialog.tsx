06f42e54b72faaea729dcd1ef39a85f29ba5c4db
kixixixixi <kixixixixi@gmail.com>
2024-07-09 01:13:41 +0900

    Fix image dialog layout

--- components/modules/image-dialog.tsx
+++ components/modules/image-dialog.tsx
@@ -37,83 +37,88 @@ export const ImageDialog: FC<
           }}
           onClick={() => setIsOpenDialog(true)}
         ></figure>
-        <dialog
-          open={isOpenDialog}
-          style={{
-            border: "none",
-            boxShadow: "0 0 0 100vmax rgba(0, 0, 0, 0.3)",
-            blockSize: "min(42rem, 80vh)",
-            inlineSize: "min(42rem, 90vw)",
-            inset: 0,
-            margin: "auto",
-            overflow: "scroll",
-            padding: "1rem",
-            placeContent: "center",
-          }}
-        >
-          <figure
+        {isOpenDialog && (
+          <dialog
+            open={isOpenDialog}
             style={{
-              backgroundImage: `url(${image.base64})`,
-              backgroundPosition: "center",
-              backgroundSize: "contain",
-              backgroundRepeat: "no-repeat",
-              height: "80%",
-              width: "100%",
-            }}
-          ></figure>
-          <Map
-            latitude={image.latitude}
-            longitude={image.longitude}
-            name={comment}
-            onChangePosition={(position) => {
-              setPosition({ latitude: position.lat, longitude: position.lng })
-            }}
-          />
-          <div style={{ padding: ".5rem" }}>
-            <Input
-              placeholder="コメント"
-              style={{ width: "100%" }}
-              value={comment}
-              onChange={({ target: { value } }) => setComment(value)}
-            />
-          </div>
-          <div
-            style={{
-              display: "flex",
-              gap: ".5rem",
-              justifyContent: "center",
+              border: "none",
+              boxShadow: "0 0 0 100vmax rgba(0, 0, 0, 0.3)",
+              blockSize: "min(42rem, 80vh)",
+              inlineSize: "min(42rem, 90vw)",
+              inset: 0,
+              margin: "auto",
+              overflow: "scroll",
+              padding: "1rem",
+              placeContent: "center",
             }}
           >
-            <Button
-              type="button"
-              onClick={async () => {
-                if (image.id) await ky.delete(`/api/image/${image.id}`)
-                await onDelete()
+            <figure
+              style={{
+                backgroundImage: `url(${image.base64})`,
+                backgroundPosition: "center",
+                backgroundSize: "contain",
+                backgroundRepeat: "no-repeat",
+                height: "80%",
+                width: "100%",
+              }}
+            ></figure>
+            <Map
+              latitude={image.latitude}
+              longitude={image.longitude}
+              name={comment}
+              onChangePosition={(position) => {
+                setPosition({ latitude: position.lat, longitude: position.lng })
+              }}
+            />
+            <div style={{ padding: ".5rem" }}>
+              <Input
+                placeholder="コメント"
+                style={{ width: "100%" }}
+                value={comment}
+                onChange={({ target: { value } }) => setComment(value)}
+              />
+            </div>
+            <div
+              style={{
+                display: "flex",
+                gap: ".5rem",
+                justifyContent: "center",
               }}
             >
-              削除
-            </Button>
-            {image.id && (
               <Button
                 type="button"
                 onClick={async () => {
-                  if (image.id) {
-                    const response = await ky.patch(`/api/image/${image.id}`, {
-                      json: { comment, ...position },
-                    })
-                    await response.json<{ image: Image }>()
-                    setIsOpenDialog(false)
-                  }
+                  if (image.id) await ky.delete(`/api/image/${image.id}`)
+                  await onDelete()
                 }}
               >
-                更新
+                削除
               </Button>
-            )}
-            <Button type="button" onClick={() => setIsOpenDialog(false)}>
-              閉じる
-            </Button>
-          </div>
-        </dialog>
+              {image.id && (
+                <Button
+                  type="button"
+                  onClick={async () => {
+                    if (image.id) {
+                      const response = await ky.patch(
+                        `/api/image/${image.id}`,
+                        {
+                          json: { comment, ...position },
+                        }
+                      )
+                      await response.json<{ image: Image }>()
+                      setIsOpenDialog(false)
+                    }
+                  }}
+                >
+                  更新
+                </Button>
+              )}
+              <Button type="button" onClick={() => setIsOpenDialog(false)}>
+                閉じる
+              </Button>
+            </div>
+          </dialog>
+        )}
       </div>
     </>
   )
