c805664e22eb2a93e4ef9bb725593aed3c2383cf
kixixixixi <kixixixixi@gmail.com>
2024-11-21 17:15:48 +0900

    Fix signin

--- components/main.tsx
+++ components/main.tsx
@@ -1,11 +1,28 @@
 "use client"
 
-import { accountAtom, useAtomValue } from "@/lib/store"
-import { ComponentProps, FC } from "react"
+import { accountAtom, useAtom } from "@/lib/store"
+import { ComponentProps, FC, FormEvent, useState } from "react"
 import { Container } from "@/components/elements"
+import { Button, Field, Input } from "@/components/elements"
+import { supabase } from "@/lib/supabase"
 
 export const Main: FC<ComponentProps<"main">> = ({ children, ...props }) => {
-  const account = useAtomValue(accountAtom)
+  const [account, setAccount] = useAtom(accountAtom)
+  const [params, setParams] = useState<{ email: string; password: string }>({
+    email: "",
+    password: "",
+  })
+  const handleSignIn = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    const {
+      error,
+      data: { user },
+    } = await supabase.auth.signInWithPassword({
+      ...params,
+    })
+    if (error) return
+    if (user) setAccount(user)
+  }
   return (
     <main
       style={{
@@ -24,11 +41,41 @@ export const Main: FC<ComponentProps<"main">> = ({ children, ...props }) => {
               style={{
                 alignItems: "center",
                 display: "flex",
+                flexFlow: "column",
                 height: "100%",
                 justifyContent: "center",
               }}
             >
-              <h1>ログインしてください</h1>
+              <h1
+                style={{
+                  padding: "2rem 0",
+                }}
+              >
+                公共住宅点検アプリ
+              </h1>
+              <form onSubmit={handleSignIn}>
+                <Field>
+                  <label>Email</label>
+                  <Input
+                    type="email"
+                    value={params.email}
+                    onChange={({ target: { value } }) =>
+                      setParams({ ...params, email: value })
+                    }
+                  />
+                </Field>
+                <Field>
+                  <label>Password</label>
+                  <Input
+                    type="password"
+                    value={params.password}
+                    onChange={({ target: { value } }) =>
+                      setParams({ ...params, password: value })
+                    }
+                  />
+                </Field>
+                <Button>ログインする</Button>
+              </form>
             </div>
           </>
         )}
--- components/modules/sign-in-menu.tsx
+++ components/modules/sign-in-menu.tsx
@@ -1,22 +1,16 @@
 "use client"
 
-import { FormEvent, useState, type ComponentProps, type FC } from "react"
-import { Button, Field, Input } from "../elements"
+import { type ComponentProps, type FC } from "react"
+import { Button } from "../elements"
 import { supabase } from "@/lib/supabase"
 import { useEffect } from "react"
 import { accountAtom, useAtom } from "@/lib/store"
-import { Dialog } from "../elements/dialog"
 
 export const SignInMenu: FC<ComponentProps<"div"> & {}> = ({
   style,
   ...props
 }) => {
   const [account, setAccount] = useAtom(accountAtom)
-  const [isOpenSignInModal, setIsOpenSignInModal] = useState<boolean>(false)
-  const [params, setParams] = useState<{ email: string; password: string }>({
-    email: "",
-    password: "",
-  })
   useEffect(() => {
     const signIn = async () => {
       const {
@@ -29,12 +23,6 @@ export const SignInMenu: FC<ComponentProps<"div"> & {}> = ({
     if (!account) signIn()
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
-  const handleSignIn = async (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    const { error } = await supabase.auth.signInWithPassword({
-      ...params,
-    })
-  }
 
   return (
     <div
@@ -60,37 +48,7 @@ export const SignInMenu: FC<ComponentProps<"div"> & {}> = ({
           </Button>
         </>
       ) : (
-        <>
-          <Button onClick={() => setIsOpenSignInModal(true)}>ログイン</Button>
-          <Dialog
-            onClose={() => setIsOpenSignInModal(false)}
-            open={isOpenSignInModal}
-          >
-            <form onSubmit={handleSignIn}>
-              <Field>
-                <label>Email</label>
-                <Input
-                  type="email"
-                  value={params.email}
-                  onChange={({ target: { value } }) =>
-                    setParams({ ...params, email: value })
-                  }
-                />
-              </Field>
-              <Field>
-                <label>Password</label>
-                <Input
-                  type="password"
-                  value={params.password}
-                  onChange={({ target: { value } }) =>
-                    setParams({ ...params, password: value })
-                  }
-                />
-              </Field>
-              <Button>ログインする</Button>
-            </form>
-          </Dialog>
-        </>
+        <></>
       )}
     </div>
   )
