461d1958d8dcad2db0fe302184a59e83eecd08f7
kixixixixi <kixixixixi@gmail.com>
2024-12-26 03:47:15 +0900

    Add house api auth

--- app/api/house/[id]/route.ts
+++ app/api/house/[id]/route.ts
@@ -1,15 +1,21 @@
 import { NextRequest, NextResponse } from "next/server"
 import { prisma } from "lib/db"
 import { House, Prisma } from "@prisma/client"
+import { authenticate } from "@/lib/authenticate"
 
 export const GET = async (
   request: NextRequest,
   { params: { id } }: { params: { id: string } }
-): Promise<NextResponse<{ house: House }>> => {
+): Promise<NextResponse<{ house: House } | { message: string }>> => {
+  const user = await authenticate(request)
+  if (!user)
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 })
   const house = await prisma.house.findUniqueOrThrow({
     where: { id: parseInt(id) },
     include: { units: true, versions: true },
   })
+  if (house.teamId !== user.teamId)
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 })
   return NextResponse.json({ house })
 }
 
@@ -17,11 +23,16 @@ export const PATCH = async (
   request: NextRequest,
   { params: { id } }: { params: { id: string } }
 ): Promise<NextResponse<{ house: House } | { message: string }>> => {
+  const user = await authenticate(request)
+  if (!user)
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 })
   const house = await prisma.house.findUniqueOrThrow({
     where: { id: parseInt(id) },
   })
   if (!house)
     return NextResponse.json({ message: "Not found" }, { status: 404 })
+  if (house.teamId !== user.teamId)
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 })
   const body: Prisma.HouseUpdateInput = await request.json()
   const updatedHouse = await prisma.house.update({
     where: { id: parseInt(id) },
@@ -35,6 +46,11 @@ export const DELETE = async (
   request: NextRequest,
   { params: { id } }: { params: { id: string } }
 ): Promise<NextResponse<{ message: string }>> => {
-  await prisma.house.delete({ where: { id: parseInt(id) } })
+  const user = await authenticate(request)
+  if (!user)
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 })
+  await prisma.house.delete({
+    where: { id: parseInt(id), teamId: user.teamId },
+  })
   return NextResponse.json({ message: "削除しました" })
 }
